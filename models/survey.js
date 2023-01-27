17ba0e765bc58f45fbc2416f3e514746d7e02e76
Reuben Ngururu <RNGURURU@safaricom.co.ke>
2023-01-27 09:16:35 +0300

    modify survey.js file

---
+++ models/survey.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Sgitchema;
+
+let surveySchema = schemaDetails ({
+    name: 'string',
+    phoneNumber: Number,
+    gender: 'string',
+    birthday: 'string',
+    employmentStatus: 'string',
+    networth: 'string',
+    do_you_own_a_home: 'string'
+
+});
+
+module.exports = schemaDetails;
